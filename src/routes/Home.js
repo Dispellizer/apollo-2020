5003a3d1367d26bc5838a0226851d7d356d97453
dispellizer <dispellizer@naver.com>
2020-04-08 21:30:17 +0900

    1.1 GET_MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+// 쿼리 작성 방법
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  //   쿼리 사용 방법
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
